f4d949d4b47acc04fb649ce5e96fcc389bd93474
harvey <im.harveywong@outlook.com>
2022-10-16 21:21:51 +0800

    in main on test rebase branch

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("in main branch!--new work");
-        System.out.println("in main branch!--new work");
-        System.out.println("in main branch!--new work");
+        System.out.println("in main branch!--in main on test rebase branch");
     }
 }
 
