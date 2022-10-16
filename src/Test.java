4a12e49e303b689de99f7854aee8db1fda9192a1
harvey <im.harveywong@outlook.com>
2022-10-16 21:19:41 +0800

    feat:add rebase lines ...

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("in main branch!--new work");
         System.out.println("in main branch!--new work");
-        System.out.println("in main branch!--new work");
+        System.out.println("in main branch!--test rebase...");
     }
 }
 
