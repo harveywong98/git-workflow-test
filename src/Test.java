33c3f7fa855b1aeafa1920268ecbfbff83782ec9
harvey <im.harveywong@outlook.com>
2022-10-16 22:19:52 +0800

    feat:rebase test

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,8 @@ public class Test {
         System.out.println("in main branch!--new work");
         System.out.println("in main branch!--new work");
         System.out.println("in main branch!--test rebase...");
+        System.out.println("in main branch!--test rebase...2");
+        System.out.println("in main branch!--test rebase...3");
     }
 }
 
